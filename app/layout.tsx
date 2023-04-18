bfe96bf7da89db9ba03c56095192ed5ec490ff4e
kenkay <kenanikay@gmail.com>
2023-04-18 04:07:32 +0300

    vercel deploy worked better with yarn

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,19 @@ import './globals.css'
 import { ThemeProvider } from 'next-themes'
 import Footer from '@/components/Footer'
 import { Poppins } from 'next/font/google'
-import { Metadata } from 'next';
+//import { Metadata } from 'next';
 
 
 const poppins = Poppins({ subsets: ['devanagari', 'latin', 'latin-ext'], 
 weight: [ '100', '200', '300', '400', '500', '600', '700', '800', '900' ] })
 
 
-
-export const metadata: Metadata = {
+/*
+export const metadata = {
   title: 'Kenani',
   description: 'kenani.dev',
 }
+*/
 
 export default function RootLayout({
   children,
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,8 +1,4 @@
-
-
 import Works from "@/components/Works"
-
-
 
 export default function WorksPage() {
     return (
